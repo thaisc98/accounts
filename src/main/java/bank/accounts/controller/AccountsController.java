dd380904460cce6ed7e863cafb472d6185488990
thaisc98 <thaisc98@hotmail.es>
2022-12-14 11:38:21 -0400

    accounts properties

--- src/main/java/bank/accounts/controller/AccountsController.java
+++ src/main/java/bank/accounts/controller/AccountsController.java
@@ -1,9 +1,15 @@
 package bank.accounts.controller;
 
+import bank.accounts.config.AccountsServiceConfig;
 import bank.accounts.model.Accounts;
 import bank.accounts.model.Customer;
+import bank.accounts.model.Properties;
 import bank.accounts.repository.AccountsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +21,9 @@ public class AccountsController {
 
     private AccountsRepository accountsRepository;
 
+    @Autowired
+    AccountsServiceConfig accountsServiceConfig;
+
     public AccountsController(AccountsRepository accountsRepository){
         this.accountsRepository = accountsRepository;
     }
@@ -23,6 +32,14 @@ public class AccountsController {
     public Accounts getAccountsDetails(@RequestBody Customer customer){
         Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
         return Objects.nonNull(accounts) ? accounts : null;
+    }
+
+    @GetMapping("/account/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow =  new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(accountsServiceConfig.getMsg(), accountsServiceConfig.getBuildVersion(),
+                accountsServiceConfig.getMailDetails(), accountsServiceConfig.getActiveBranches());
+        return ow.writeValueAsString(properties);
     }
 
 }
