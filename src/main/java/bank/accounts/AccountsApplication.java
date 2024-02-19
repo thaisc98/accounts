8413645d42fb7b1ee9307708fd57340f4d8a72e7
thaisc98 <thaisc98@hotmail.es>
2024-02-19 17:31:23 -0400

    [NEW] OpenAPI documented

--- src/main/java/bank/accounts/AccountsApplication.java
+++ src/main/java/bank/accounts/AccountsApplication.java
@@ -7,8 +7,6 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
