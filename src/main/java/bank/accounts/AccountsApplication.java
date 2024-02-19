c42bb67c8d67295f906b8a7b4f47ff00ebc1e365
thaisc98 <thaisc98@hotmail.es>
2024-02-19 17:31:02 -0400

    [NEW] OpenAPI documented

--- src/main/java/bank/accounts/AccountsApplication.java
+++ src/main/java/bank/accounts/AccountsApplication.java
@@ -1,5 +1,10 @@
 package bank.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -10,6 +15,26 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories("bank.accounts.repository")
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title= "Accounts microservice REST API Documentation",
+				description= "Bank Accounts microservices REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Thais Contreras",
+						email = "tutor@bank.com"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Bank Accounts microservices REST API Documentation",
+				url ="http://localhost:8080/swagger-ui/index.html#/"
+		)
+)
+
 public class AccountsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/bank/accounts/controller/AccountsController.java
+++ src/main/java/bank/accounts/controller/AccountsController.java
@@ -4,6 +4,10 @@ import bank.accounts.constants.AccountsConstants;
 import bank.accounts.dto.CustomerDto;
 import bank.accounts.dto.ResponseDto;
 import bank.accounts.service.AccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -14,6 +18,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+    name = "Accounts in Bank",
+    description = "CRUD REST APIs in Bank to CREATE,UPDATE, FETCH AND DELETE account details"
+)
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
@@ -22,6 +30,14 @@ public class AccountsController {
 
     private AccountsService accountsService;
 
+    @Operation(
+            summary = "Create Account",
+            description = "Create a new Customer & Account inside Bank"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status Created"
+    )
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
         accountsService.createAccount(customerDto);
@@ -31,6 +47,14 @@ public class AccountsController {
     }
 
 
+    @Operation(
+            summary = "Fetch Account Details",
+            description = "Fetch Customer & Account details based on mobile number"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status Ok"
+    )
     @GetMapping("/fetch")
     public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam
                                                            @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
@@ -40,6 +64,20 @@ public class AccountsController {
         return ResponseEntity.status(HttpStatus.OK).body(customerDto);
     }
 
+    @Operation(
+            summary = "Update Account Details",
+            description = "Update Customer & Account details based on mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status Ok"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    })
     @PutMapping("/update")
     public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) {
         boolean isUpdated = accountsService.updateAccount(customerDto);
@@ -54,6 +92,20 @@ public class AccountsController {
         }
     }
 
+    @Operation(
+            summary = "Delete Account Details",
+            description = "Delete Customer & Account details based on mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status Ok"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    })
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam
                                                             @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
